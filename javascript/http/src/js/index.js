60cd6044c74a6fc08fb2e1d19e41e94f4e5ce4ea
deekshi-1 <deekshithc1@gmail.com>
2024-08-25 14:49:01 +0530

    http

--- javascript/http/src/js/index.js
+++ javascript/http/src/js/index.js
@@ -9,6 +9,17 @@ const Row = ({ product, price }) =>
     <span class="prod-cost">$${price}</span>
   </div>`;
 
+const getData = (uri) =>
+  new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', function () {
+      resolve(JSON.parse(this.response));
+    });
+    xhr.addEventListener('error', () => reject());
+    xhr.open('GET', uri);
+    xhr.send();
+  });
+
 const render = function (arr) {
   const elems = arr.map((e) => Row(e));
   const totalCost = arr.reduce((prev, curr) => prev + Number(curr.price), 0);
@@ -16,3 +27,12 @@ const render = function (arr) {
   container.innerHTML = elems.join('');
   total.innerHTML = Row({ product: 'TOTAL', price: totalCost });
 };
+
+loadCartBtn.addEventListener('click', async () => {
+  try {
+    const products = await getData('http://localhost:3000/products');
+    render(products);
+  } catch {
+    alert('There was an error talking to the server!');
+  }
+});
