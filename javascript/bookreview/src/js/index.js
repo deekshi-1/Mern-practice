86a73e435f1b49acfbc234ffa2efc018eaa02b26
deekshi-1 <deekshithc1@gmail.com>
2024-08-27 21:35:14 +0530

    removeButton

--- javascript/bookreview/src/js/index.js
+++ javascript/bookreview/src/js/index.js
@@ -1,15 +1,68 @@
-// The following code starts Mock Service Worker tool which allows you to simulate a backend (an API) for building your apps that talk to a remote service. You can visit https://mswjs.io for details on this utility and check src/api/routes.js for a sample API route that you can edit/create as needed to simulate a real world API. This simulated backend will not be part of the completed application (built edition) and you must use a real world backend built using Node.js + Express or Java + Spring Boot to provide such a service.
+import Item from "../../../shoppinglist/src/js/Item";
 
-// If you do not require a simulated backend, you can remove the code shown below.
-
-const apiStatus = document.querySelector('#api-status');
-
-if (import.meta.env.DEV) {
-  import('../api/browser')
-    .then(({ worker }) => worker.start())
-    .then(() => fetch('/'))
-    .then((res) => res.json())
-    .then((res) => (apiStatus.innerText = res.message));
+function Book(title, author, rating) {
+  this.title = title;
+  this.author = author;
+  this.id = Date.now().toString();
+  this.rating = rating;
+  this.reviews = [];
 }
 
+const BookCollection = {
+  books: [],
+  addBook: function (book) {
+    let bookTitle = document.getElementById("bookTitle").value;
+    let bookAuthor = document.getElementById("bookAuthor").value;
+    let bookRating = document.getElementById("bookRating").value;
+    let abook =new Book(bookTitle,bookAuthor,bookRating );
+    this.books.push(abook);
+    BookCollection.render();
+    console.log(this.books);
+    rmAttach();
+  },
+  removeBook: function (rmid) {
+    let bookToRemoved = this.books.findIndex((book) => book.id === rmid);
+    if (bookToRemoved ==-1){}
+    else{
+      let removeBook = this.books.splice(bookToRemoved, 1);
+      console.log(removeBook);
+      this.render();
+      
+    }
+    this.render();
+  },
+  searchBook: function (title) {
+    let filterBook = this.book.filter((book) => book.title.includes(title));
+    if (filterBook != 0) {
+    }
+  },
+  render: function () {
+    let item = "";
+    BookCollection.books.forEach((element) => {
+      let bookDisplay = `<div>
+  <h3>Title:${element.title}</h3>
+  <h4>Author:${element.author}</h4>
+  <p>Rating:${element.rating}</p>
+  <button class="removeButton" id="${element.id}">Remove</button>
+  </div>`;
+      item += bookDisplay;
+    });
+    document.getElementById("showBook").innerHTML = item;
+  },
+};
 
+let addButton = document.getElementById("submitButton");
+addButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  BookCollection.addBook();
+});
+
+function rmAttach() {
+  let rmButton = document.querySelectorAll(".removeButton");
+  rmButton.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      console.log(btn.id);
+      BookCollection.removeBook(btn.id);
+    });
+  });
+};
